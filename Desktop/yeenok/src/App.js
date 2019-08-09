11a86ece9d0b9997e25fda40d12ec230d27e65c0
luongviettuan <congiolanhlungtuan@gmail.com>
2019-08-09 09:03:27 +0700

    init

---
+++ Desktop/yeenok/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.css';
+
+import './public/js/jQuery_v3.1.1.min.js';
+import './public/js/owl.carousel.min.js';
+import './public/js/bootstrap.min.js';
+import './public/js/jquery.magnific-popup.js';
+import './public/js/jquery.firstVisitPopup.js';
+import './public/js/custom.js';
+
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Home />
+    </div>
+  );
+}
+
+export default App;
