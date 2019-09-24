fedbe2ce2ea557528f98aa1e87add382cb28f9b5
luongviettuan <congiolanhlungtuan@gmail.com>
2019-09-25 00:00:08 +0700

    fix cors

--- Desktop/Server/index.js
+++ Desktop/Server/index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    next();
+});
 
 var UserRouter = require('./router/User.Router')
 app.use('/user', UserRouter)
