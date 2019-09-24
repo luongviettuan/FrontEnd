ddf4e32fe1030dcb5667c24136de0a97bdc6ce97
luongviettuan <congiolanhlungtuan@gmail.com>
2019-09-24 19:32:10 +0700

    getAllCategory

--- Desktop/Server/controller/Category.Controller.js
+++ Desktop/Server/controller/Category.Controller.js
@@ -1,4 +1,16 @@
 const conn = require('../model/config')
+module.exports.getAllCategory = (req, res, next) =>{
+    let sql = `select * from Category;`
+    conn.query(sql, (err, rs)=>{
+        if(err) throw err;
+        else{
+            res.json({
+                code: 200,
+                result: rs
+            })
+        }
+    })
+}
 module.exports.getProductOfCategory = (req, res, next) =>{
     let categoryId = req.params.categoryId;
     let sql = `select * from Product where category_id = "${categoryId}";`
--- Desktop/Server/router/Category.Router.js
+++ Desktop/Server/router/Category.Router.js
@@ -2,6 +2,7 @@ const express = require('express')
 var router = express.Router()
 
 const CategoryController = require('../controller/Category.Controller')
+router.get('/', CategoryController.getAllCategory);
 router.get('/:categoryId', CategoryController.getProductOfCategory)
 
 module.exports  = router;
