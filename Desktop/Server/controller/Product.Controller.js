304f627fe89936d447cfeb6c8aea670a68928674
luongviettuan <congiolanhlungtuan@gmail.com>
2019-10-03 09:23:28 +0700

    size

--- Desktop/Server/controller/Product.Controller.js
+++ Desktop/Server/controller/Product.Controller.js
@@ -1,4 +1,5 @@
 const conn = require('../model/config')
+const HelpUtil = require('../util/Help.Util')
 module.exports.getAllProduct = (req, res, next)=>{
     let sql = "select product_name, product_image_url, price from Product";
     conn.query(sql, (err, rs)=>{
@@ -20,14 +21,18 @@ module.exports.searchProduct = (req, res, next)=>{
 }
 module.exports.getProductById = (req, res, next)=>{
     let productId = req.params.productId;
-    let sql = `SELECT * FROM Product WHERE product_id = ${productId}`
+    let sql = `SELECT * FROM queenok.Product INNER JOIN Brand ON Product.brand_id = Brand.brand_id WHERE product_id  = ${productId}`
     conn.query(sql, (err, rs)=>{
         if(err) throw err;
         else{
+            let size = HelpUtil.stringToArray(rs[0].size);
+            let color = HelpUtil.stringToArray(rs[0].color);
+            let resutl = {...rs[0], size : size, color : color}
             res.json({
                 code : 200,
-                result : rs
+                result : resutl
             })
+            
         }
     })
     
