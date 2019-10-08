28e928e35e566d51a6062dcded8726de6c8786ea
luongviettuan <congiolanhlungtuan@gmail.com>
2019-10-08 23:16:01 +0700

    target value regiter

--- Desktop/yeenok/src/components/Regitster_Form.js
+++ Desktop/yeenok/src/components/Regitster_Form.js
@@ -7,8 +7,20 @@ export default class LoginForm extends Component{
         this.state = {
             province : [],
             district : [],
+            user : {
+                fullName : "",
+                phoneNumber : "",
+                provinceId : "",
+                districtId : "",
+                addressDetail : "",
+                username : "",
+                password : "",
+                confirmPassword: ""
+            }
         }
+
         this.handleChange = this.handleChange.bind(this)
+        this.handleLogin = this.handleLogin.bind(this)
     }
     componentDidMount(){
         axios.get('http://localhost:8080/location/province')
@@ -18,19 +30,32 @@ export default class LoginForm extends Component{
                 })
             })
     }
-    handleChange(){
+    handleChange(event){
         if(this.refs.province_ref.value != ""){
-            axios.post("http://localhost:8080/location/province", {province_id : this.refs.province_ref.value})
+            axios.post("http://localhost:8080/location/province",
+                        {province_id : this.refs.province_ref.value}
+                    )
             .then(res=>{
                 this.setState({
                     district : res.data.result
                 })
             })
         }
+        this.setState({user : {
+            ...this.state.user,
+            [event.target.name] : event.target.value,
+            }
+        })
+    }
+
+    handleLogin(event){
+        axios.post("http://localhost:8080/user/register", this.state.user).then(res =>{
+            console.log (res.data.message)
+        })
+        event.preventDefault();
     }
     render(){
-        console.log(this.state.province);
-        
+
         return(
             <div className="col-sm-8 col-lg-9 mtb_20">
                 <div className="row">
@@ -50,18 +75,18 @@ export default class LoginForm extends Component{
                             <div className="panel-body">
                                 <div className="row">
                                     <div className="col-lg-12">
-                                        <form id="login-form" action="#" method="post">
+                                        <form id="login-form" method="post" onSubmit={this.handleLogin}>
                                             <div className="form-group">
                                                 <label>Họ Tên</label>
-                                                <input type="text" name="fullName" tabIndex="1" className="form-control" placeholder="Họ Tên" />
+                                                <input type="text" name="fullName" tabIndex="1" className="form-control" placeholder="Họ Tên" value={this.state.fullName} onChange={this.handleChange}/>
                                             </div>
                                             <div className="form-group">
                                                 <label>Số Điện Thoại</label>
-                                                <input type="text" name="phoneNumber" tabIndex="1" className="form-control" placeholder="Số Điện Thoại" />
+                                                <input type="text" name="phoneNumber" tabIndex="1" className="form-control" placeholder="Số Điện Thoại" value={this.state.phoneNumber} onChange={this.handleChange}/>
                                             </div>
                                             <div className="form-group">
                                                 <label>Tỉnh</label>
-                                                <select className="form-control" onChange={this.handleChange} ref="province_ref">
+                                                <select className="form-control" name="provinceId" onClick={this.handleChange} ref="province_ref" value= {this.state.province_id}>
                                                     <option value="">--- Mời Khách Hàng Chọn Tỉnh ---</option>
                                                     {
                                                         this.state.province.length > 0 && this.state.province.map(item =>
@@ -73,7 +98,7 @@ export default class LoginForm extends Component{
                                             </div>
                                             <div className="form-group">
                                                 <label>Huyện</label>
-                                                <select className="form-control">
+                                                <select className="form-control" name="districtId" onChange={this.handleChange} value={this.state.district_id}>
                                                     {
                                                         this.state.district.length >  0 ? this.state.district.map(item =>
                                                             <option 
@@ -86,24 +111,24 @@ export default class LoginForm extends Component{
                                             </div>
                                             <div className="form-group">
                                                 <label>Địa Chỉ Chi Tiết</label>
-                                                <input type="text" name="addressDetail" tabIndex="1" className="form-control" placeholder="Xã, Thôn, Xóm, Làng, Đường, Số Nhà ..." />
+                                                <input type="text" name="addressDetail" tabIndex="1" className="form-control" placeholder="Xã, Thôn, Xóm, Làng, Đường, Số Nhà ..." onChange= {this.handleChange} value={this.state.addressDetail}/>
                                             </div>
                                             <div className="form-group">
                                                 <label>Tên Đăng Nhập</label>
-                                                <input type="text" name="username" tabIndex="1" className="form-control" placeholder="Tên Đăng Nhập" />
+                                                <input type="text" name="username" tabIndex="1" className="form-control" placeholder="Tên Đăng Nhập" onChange ={this.handleChange} value ={this.state.username}/>
                                             </div>
                                             <div className="form-group">
                                                 <label>Mật Khẩu</label>
-                                                <input type="password" name="password" tabIndex="2" className="form-control" placeholder="Nhập Mật Khẩu" />
+                                                <input type="password" name="password" tabIndex="2" className="form-control" placeholder="Nhập Mật Khẩu" onChange={this.handleChange} value={this.state.password} />
                                             </div>
                                             <div className="form-group">
                                                 <label>Nhập Lại Mật Khẩu</label>
-                                                <input type="password" name="confirmPassword" tabIndex="2" className="form-control" placeholder="Xác Nhận Mật Khẩu" />
+                                                <input type="password" name="confirmPassword" tabIndex="2" className="form-control" placeholder="Xác Nhận Mật Khẩu" onChange={this.handleChange} value={this.state.confirmPassword}/>
                                             </div>
                                             <div className="form-group">
                                                 <div className="row">
                                                     <div className="col-sm-6 col-sm-offset-3">
-                                                        <input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Đăng Ký" />
+                                                        <input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Đăng Ký"/>
                                                     </div>
                                                 </div>
                                             </div>
